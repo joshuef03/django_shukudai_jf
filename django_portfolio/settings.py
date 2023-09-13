7825f1d962cec22f500577dd3f5f154825a46ab0
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-12 21:45:40 -0500

    commit 2.1

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -28,7 +28,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-7&avfwsx%6y)x
 DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
-
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:    
 	ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
