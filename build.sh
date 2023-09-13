ddaded974ade8c6a8c0eef38459c9151bad3ed36
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-12 23:26:09 -0500

    superuser creation2.2.1

--- build.sh
+++ build.sh
@@ -2,13 +2,14 @@
 # exit on error
 set -o errexit
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
 
 pip install -r requirements.txt
 
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
