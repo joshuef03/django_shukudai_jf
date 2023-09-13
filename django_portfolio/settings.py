4bcde7a21003ff76bde14e4e221cc1f8955e7c11
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-12 22:19:48 -0500

    commit 2.2

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-7&avfwsx%6y)xuez*n!ltf#qb(9ds*ecj$4ox_2d2b4w-!__pq')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
