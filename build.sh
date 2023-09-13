8504d113989a748587d955a61f0efdc427acabca
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-12 23:13:59 -0500

    superuser creation

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python django_portfolio/manage.py createsuperuser --no-input
+fi
+
 python manage.py collectstatic --no-input
 python manage.py migrate
