bb2a403d90f06cf1d2cf28c0b345037154d47b4a
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-13 01:02:54 -0500

    finalizacion 1.2

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -134,8 +134,8 @@ if not DEBUG:
     # Turn on WhiteNoise storage backend that takes care of compressing static files
     # and creating unique names for each version so they can safely be cached forever.
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = '/public/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
