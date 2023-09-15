484e9491cdac7c336afe7d0ab2dc0e5d6132b9a5
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-15 01:06:50 -0500

    mediaurls

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('blog/',include('blog.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
