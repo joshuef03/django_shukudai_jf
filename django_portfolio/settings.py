01bac4adee1285e1ee9572bd1d799a423570f500
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-17 15:32:46 -0500

    hwga0.1.1

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'django_portfolio.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/django_portfolio_bd',
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
