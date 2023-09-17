d2d89c4574b25b5b52197a9c6bd1e8700e624560
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-16 22:18:00 -0500

    anotherpath1.1.1

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,5 @@ import datetime
 class Post(models.Model):
     title=models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(upload_to= 'media/blog/images')
+    image = models.ImageField(upload_to= 'blog/images')
     date =models.DateField(datetime.date.today)
