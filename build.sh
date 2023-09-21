f967d68611768411b5604a8275c48370c7f93101
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-20 23:45:54 -0500

    hwga0.1.5

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -o errexit
 
 pip install -r requirements.txt
 
+python manage.py collectstatic --no-input
+python manage.py migrate
+
 chmod -R 755 media/
 echo "location /public/ {
     alias /media/;
 }" > /etc/nginx/conf.d/media.conf
 service nginx restart
-
-python manage.py collectstatic --no-input
-python manage.py migrate
 
 if [[ $CREATE_SUPERUSER ]];
 then
