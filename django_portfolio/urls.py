381d987e89b44f864a4d6b3db4c2f21c87531480
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-21 17:57:15 -0500

    hwga0.2.0.3

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -19,7 +19,6 @@ from django.urls import include,path
 from django.conf.urls.static import static
 from django.conf import settings
 from portfolio import views
-from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
