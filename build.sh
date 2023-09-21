ea34b166436930a566a549609070b2f4a7cc5949
joshuef03 <e1316182094@live.uleam.edu.ec>
2023-09-20 23:43:02 -0500

    hwga0.1.4

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+chmod -R 755 media/
+echo "location /public/ {
+    alias /media/;
+}" > /etc/nginx/conf.d/media.conf
+service nginx restart
 
 python manage.py collectstatic --no-input
 python manage.py migrate
